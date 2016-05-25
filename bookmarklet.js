293a983476fd6d400e55e826bd61ad22ad453a42
psflannery <psflannery@gmail.com>
2016-05-25 23:29:38 +0100

    bookmarklet and tidy

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,32 +1,17 @@
-javascript: (function () { 
-    var jsCode = document.createElement('script');
-    var styles = document.createElement('link');
-    
-    jsCode.setAttribute('src', 'http://www.website.com/js/main.js');
-    styles.setAttribute('rel', 'stylesheet');
-    styles.setAttribute('href', 'http://www.website.com/css/style.css');
-    document.body.appendChild(jsCode); 
-}());
-
-/*
-javascript:var i,s,ss=[
-    'http://kathack.com/js/kh.js',
-    'http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'
-];
-for(i=0;i!=ss.length;i++){
-    s=document.createElement('script');
-    s.src=ss[i];
-    document.body.appendChild(s);
-}void(0);
-*/
-
-/*
 javascript: (function () {
-	var linkNode = document.createElement('link');
-	linkNode.rel = 'stylesheet';
-	linkNode.href = 'http://www.website.com/stylesheets/style.css';
-	document.getElementsByTagName('head')[0].appendChild(linkNode);
-})();
-*/
-
-// http://code.tutsplus.com/tutorials/create-bookmarklets-the-right-way--net-18154
+    var i,
+        scripts,
+        styles = document.createElement('link'),
+        jsCode = [
+            'https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.4.1/snap.svg-min.js',
+            'https://dl.dropboxusercontent.com/u/2250499/opening_times/Takeovers/RA/demos/raw-demo/js/main.js'
+        ];
+    styles.setAttribute('rel', 'stylesheet');
+    styles.setAttribute('href', 'https://dl.dropboxusercontent.com/u/2250499/opening_times/Takeovers/RA/demos/css/main.css');
+    document.getElementsByTagName('head')[0].appendChild(styles);
+    for(i=0; i!=jsCode.length; i++){
+        scripts = document.createElement('script');
+        scripts.setAttribute('src', jsCode[i]);
+        document.body.appendChild(scripts);
+    };
+}());
